e7c1b08b31597c3801fe24069d38f9c498575761
Celestine Stephen <decele2011@gmail.com>
2023-05-25 01:57:58 +0100

    finilization with unit tests

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TeamRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(TeamRepository $teamRepository): Response
     {
         return $this->render('pages/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'teams' => $teamRepository->createQueryBuilder('q')
+                ->orderBy('q.money_balance', 'DESC')
+                ->setMaxResults(6)
+                ->getQuery()
+                ->getResult()
         ]);
     }
 }
